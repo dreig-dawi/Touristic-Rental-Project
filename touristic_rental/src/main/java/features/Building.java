0055eec3a2a371bfbe43ae5ef7af5e76684e7157
dreig-dawi <dreigbuades@cifpfbmoll.eu>
2024-04-22 16:05:46 +0200

    Running firsts tests (#8)

--- touristic_rental/src/main/java/features/Building.java
+++ touristic_rental/src/main/java/features/Building.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Building {
     private String name;
-    private int apartments;
     private List<Apartment> apartmentList = new ArrayList<Apartment>();
 
     public Building(String name) {
@@ -17,5 +16,42 @@ public class Building {
     }
     public void addApartment(Apartment apartment) {
         this.apartmentList.add(apartment);
+    }
+    public void getApartmentQuantity() {
+        System.out.println("The building has " + this.apartmentList.size() + " apartments.");
+    }
+    public int getApartmentQuantityNumber() {
+        return this.apartmentList.size();
+    }
+    public void getApartmentNames() {
+        System.out.println("The building has the following apartments:");
+        for (Apartment apartment : this.apartmentList) {
+            System.out.println(apartment.getName());
+        }
+    }
+    public String getApartmentNamesString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("The building has the following apartments:\n");
+        for (Apartment apartment : this.apartmentList) {
+            sb.append(apartment.getName() + "\n");
+        }
+        return sb.toString();
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void getApartmentList() {
+        getApartmentQuantity();
+        for (Apartment apartment : this.apartmentList) {
+            System.out.println(apartment.getName());
+        }
+    }
+    public Apartment getApartment(String name) {
+        for (Apartment apartment : this.apartmentList) {
+            if (apartment.getName().equals(name)) {
+                return apartment;
+            }
+        }
+        return null;
     }
 }
--- touristic_rental/src/test/java/features/BuildingTest.java
+++ touristic_rental/src/test/java/features/BuildingTest.java
@@ -2,9 +2,6 @@ package features;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import features.Apartment;
-import features.Building;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingTest {
@@ -28,5 +25,36 @@ class BuildingTest {
         building.addApartment("Test Apartment 2");
         // Assuming you have a method to get an apartment by name
         assertNotNull(building.getApartment("Test Apartment 2"));
+    }
+
+    @Test
+    void testGetApartmentQuantity() {
+        building.addApartment("Apartment 1");
+        building.addApartment("Apartment 2");
+        building.addApartment("Apartment 3");
+        assertEquals(3, building.getApartmentQuantityNumber());
+    }
+
+    @Test
+    void testGetApartmentNames() {
+        building.addApartment("Apartment 1");
+        building.addApartment("Apartment 2");
+        building.addApartment("Apartment 3");
+        String expected = "The building has the following apartments:\nApartment 1\nApartment 2\nApartment 3\n";
+        assertEquals(expected, building.getApartmentNamesString());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Test Building", building.getName());
+    }
+
+    @Test
+    void testGetApartmentList() {
+        building.addApartment("Apartment 1");
+        building.addApartment("Apartment 2");
+        building.addApartment("Apartment 3");
+        String expected = "The building has 3 apartments.\nApartment 1\nApartment 2\nApartment 3\n";
+        assertEquals(expected, building.getApartmentList());
     }
 }
